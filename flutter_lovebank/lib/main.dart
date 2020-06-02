cfa485da10b34995621291eb1cc9c00ef5f11ee7
william <191marquis@gmail.com>
2020-06-02 15:29:58 -0400

    [DEV] Add custom fonts

--- flutter_lovebank/lib/main.dart
+++ flutter_lovebank/lib/main.dart
@@ -10,12 +10,13 @@ void main() {
 class LoveApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: "LoveBank",
       theme: ThemeData(
         primaryColor: Color(0xffce00e8),
         accentColor: Color(0xfff68dc7),
         backgroundColor: Color(0xff9e00ff),
+        fontFamily: 'LiberationSans',
       ),
       home: ThreePageIntro(),
     );
