42cc2cda5e5eae889caf727c54863a919d157a29
Alex Chandy <alexchandy13@gmail.com>
2020-06-08 13:23:26 -0400

    Added Three Page View for Testing

--- flutter_lovebank/lib/main.dart
+++ flutter_lovebank/lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
-void main() => runApp(new LoveApp());
+import 'three_page_intro.dart';
+import 'package:flutter/rendering.dart'; //used for debugPaintSizeEnabled
+
+void main() {
+  //debugPaintSizeEnabled=true;
+  runApp(new LoveApp());
+}
 
 class LoveApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Material(
-        child: new Center(
-          child: new Text("Love Bank"),
-        ),
+    return MaterialApp(
+      title: "LoveBank",
+      theme: ThemeData(
+        primaryColor: Color(0xffce00e8),
+        accentColor: Color(0xfff68dc7),
+        backgroundColor: Color(0xff9e00ff),
+        fontFamily: 'LiberationSans',
       ),
+      home: ThreePageIntro(),
     );
   }
 }
