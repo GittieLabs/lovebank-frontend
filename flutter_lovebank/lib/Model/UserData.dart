e2d484a10cb186c24d822966696c2515f5e70620
VvivienneZhu <vivienne.zhujy@gmail.com>
2020-06-22 15:49:36 -0700

    Wrapper to Invitation logic

--- flutter_lovebank/lib/Model/UserData.dart
+++ flutter_lovebank/lib/Model/UserData.dart
@@ -3,8 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<User> fetchUser(int userId) async {
-  final response = await http.get('http://127.0.0.1:5000/user/${userId}');
+Future<User> fetchUser(String firebaseId) async {
+  final response = await http.get('http://127.0.0.1:5000/user/firebase/$firebaseId');
   if (response.statusCode == 200) {
     return User.fromJson(json.decode(response.body));
   } else {
@@ -13,16 +13,18 @@ Future<User> fetchUser(int userId) async {
 }
 
 class User{
-  final int id;
-  final int partnerId;
+  final String id;
+  final String partnerId;
   final String email;
   final String username;
+  final String firebaseId;
 
-  User({this.id, this.partnerId, this.email, this.username});
+  User({this.id, this.partnerId, this.email, this.username, this.firebaseId});
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       id: json['id'],
+      firebaseId: json['firebase_uid'],
       partnerId: json['partner_id'],
       email: json['email'],
       username: json['username'],
--- flutter_lovebank/lib/screens/wrapper.dart
+++ flutter_lovebank/lib/screens/wrapper.dart
@@ -1,6 +1,7 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterapp/Model/UserData.dart';
 import 'package:flutterapp/screens/invitation/invite.dart';
 import 'package:provider/provider.dart';
 import 'package:flutterapp/screens/intro/three_page_intro.dart';
@@ -10,6 +11,7 @@ import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
 class Wrapper extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<FirebaseUser>(context);
@@ -18,12 +20,14 @@ class Wrapper extends StatelessWidget {
       // If user hasn't login, display intro screen
       return ThreePageIntro();
     } else {
+      Future<User> databaseUser = fetchUser(user.uid);
+
       // If user is logged in, check its partner status
-      return FutureBuilder(
-          future: jumpInvitation(),
-          builder: (context, AsyncSnapshot<bool> snapshot) {
+      return FutureBuilder<User>(
+          future: databaseUser,
+          builder: (context, snapshot) {
             if (snapshot.hasData) {
-              if (snapshot.data == false){
+              if (snapshot.data.partnerId == null) {
                 // If user hasn't been linked to a partner, display invite screen
                 return InvitePartner();
               } else {
@@ -31,34 +35,11 @@ class Wrapper extends StatelessWidget {
                 return Home();
               }
             }
-              return CircularProgressIndicator();
+            return CircularProgressIndicator();
           }
       );
-      }
-    }
-
-  // Check the partner status of the user
-  // Return true: user has a linked partner
-  // Return false: user hasn't been linked to a partner
-  Future<bool> jumpInvitation() async {
-    var userId = 41;
-    var url = 'http://127.0.0.1:5000/user/$userId';
-
-    var response = await http.get(url);
-    if (response.statusCode == 200){
-      var jsonResponse = convert.jsonDecode(response.body);
-      var linkedStatus = jsonResponse['partner_id'];
-
-      if (linkedStatus == null) {
-        return false;
-      } else {
-        return true;
-      }
-    } else {
-      throw Exception('Failed to fetch user info');
     }
   }
-
 }
 
 
